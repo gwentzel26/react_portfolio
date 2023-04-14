c84523e37a1fee48b9e6d052181e1632329e8775
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-14 11:10:15 -0400

    Created social links component

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
     <div>
        <Navbar />
        <Home />
+
+       <SocialLinks />
     </div>
   );
 }
