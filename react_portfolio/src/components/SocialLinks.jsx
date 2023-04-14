53ae832ba7a3dff930bc2d6b76c7bd1c78645f88
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-14 11:20:00 -0400

    Styles for social links div and ul

--- react_portfolio/src/components/SocialLinks.jsx
+++ react_portfolio/src/components/SocialLinks.jsx
@@ -5,7 +5,15 @@ import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const SocialLinks = () => {
   return (
-    <div>SocialLinks</div>
+    <div className='flex flex-col top-[35%] left-0 fixed'>
+        <ul className='flex justify-between items-center w-40 h-1/4 px-4 '>
+            <li>
+                <a href='' >
+                    LinkedIn <FaLinkedin /> 
+                </a>
+            </li>
+        </ul>
+    </div>
   )
 }
 
