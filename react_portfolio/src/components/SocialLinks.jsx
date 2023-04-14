ceffc9af86171fbb003e8a609ae82f04cc0a6a19
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-14 11:14:32 -0400

    Imported react icons

---
+++ react_portfolio/src/components/SocialLinks.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+
+const SocialLinks = () => {
+  return (
+    <div>SocialLinks</div>
+  )
+}
+
+export default SocialLinks
