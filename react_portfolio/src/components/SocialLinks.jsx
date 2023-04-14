d8babc038e0fe1d6f58bcfd8c14a84e19d63e2ca
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-14 11:42:31 -0400

    Added linkedin and github links

--- react_portfolio/src/components/SocialLinks.jsx
+++ react_portfolio/src/components/SocialLinks.jsx
@@ -4,12 +4,44 @@ import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const SocialLinks = () => {
+    const Links = [
+        {
+            id: 1,
+            child: (
+                <>
+               LinkedIn <FaLinkedin size={30}/>
+                </>
+            ),
+            href: 'https://www.linkedin.com/in/gavinwentzel/',
+            style: 'rounded-tr-md'
+        },
+        {
+            id: 2,
+            child: (
+                <>
+               Github <FaGithub size={30}/>
+                </>
+            ),
+            href: 'https://github.com/gwentzel26',
+            style: 'rounded-tr-md'
+        },
+        {
+            id: 3,
+            child: (
+                <>
+               LinkedIn <FaLinkedin size={30}/>
+                </>
+            ),
+            href: 'https://www.linkedin.com/in/gavinwentzel/',
+            style: 'rounded-tr-md'
+        }
+    ]
   return (
     <div className='flex flex-col top-[35%] left-0 fixed'>
-        <ul className='flex justify-between items-center w-40 h-1/4 px-4 '>
-            <li>
-                <a href='' >
-                    LinkedIn <FaLinkedin /> 
+        <ul>
+            <li className='flex justify-between items-center w-40 h-12 px-4 bg-gray-600 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300'>
+                <a href='' className='flex justify-between items-center w-full text-white'>
+                    LinkedIn <FaLinkedin size={30} /> 
                 </a>
             </li>
         </ul>
