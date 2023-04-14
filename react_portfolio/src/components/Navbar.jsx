7cf2b2468d6d2f635f1f806f42786017cac4210c
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-14 00:54:39 -0400

    Completed h2, image, and button styling

--- react_portfolio/src/components/Navbar.jsx
+++ react_portfolio/src/components/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
       </div>
 
     {nav && (
-      <ul className="flex flex-col justify-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-400">
+      <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-400">
       {links.map(({ id, link }) => (
              <li 
              key={id}
