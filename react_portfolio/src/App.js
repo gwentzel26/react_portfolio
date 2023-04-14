557b64416f0bbba45d8fb994b8394733101f2266
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-14 13:02:25 -0400

    Started About section

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
        <Navbar />
        <Home />
-
+        <About />
        <SocialLinks />
     </div>
   );
--- react_portfolio/src/components/SocialLinks.jsx
+++ react_portfolio/src/components/SocialLinks.jsx
@@ -46,15 +46,18 @@ const SocialLinks = () => {
     },
   ];
   return (
-    <div className="flex flex-col top-[35%] left-0 fixed">
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
         {Links.map(({id, child, href, style, download}) => (
-          <li key={id} className={"flex justify-between items-center w-40 h-12 px-4 bg-gray-600 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300" + " " + style}>
+          <li key={id} className={
+            "flex justify-between items-center w-40 h-12 px-4 bg-gray-600 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300" 
+            + " " + style}>
             <a
               href={href}
               className="flex justify-between items-center w-full text-white"
               download={download}
               target='_blank'
+              rel="noreferrer"
             >
               {child}
             </a>
