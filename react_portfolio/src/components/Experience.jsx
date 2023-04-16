4c0bc8b330040ba3e93fdd7cc5222a6f6d1b822e
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-16 16:56:04 -0400

    Styling for divs completed, setting up array for technologies

--- react_portfolio/src/components/Experience.jsx
+++ react_portfolio/src/components/Experience.jsx
@@ -11,18 +11,29 @@ import graphql from "../assets/Tech-Icons/graphql.png";
 
 
 const Experience = () => {
+
+    const techs = [
+        {
+            id: 1,
+            src: html,
+            title: 'HTML',
+            style: 'shadow-orange-500'
+        }
+ 
+    ]
+
   return (
     <div name="experience" className='bg-gradient-to-b from-blue-800 to-black w-full h-screen'>
-        <div className='max-w-screen-lg '>
+        <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white '>
             <div>
-                <p>Experience</p>
-                <p>These are the technologies I've worked with</p>
+                <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
+                <p className='py-6'>These are the technologies I've worked with</p>
             </div>
 
-            <div>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
                 <div>
-                    <img src={html} alt="html" />
-                    <p>HTML</p>
+                    <img src={html} alt="html" className='w-20 mx-auto' />
+                    <p className='mt-4'>HTML</p>
                 </div>
             </div>
         </div>
