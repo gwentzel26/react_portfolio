50e208863abbdcb15425050c2394d593404bb9ce
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-16 17:50:49 -0400

    styled contact form

--- react_portfolio/src/components/Contact.jsx
+++ react_portfolio/src/components/Contact.jsx
@@ -2,24 +2,25 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-        <div>
-            <div>
-                <p>Contact</p>
-                <p>Submit the form below to contact me</p>
+    <div name="contact" className='w-full h-screen bg-gradient-to-b from-black to-blue-800 p-4 text-white'>
+        <div className='flex flex-col max-w-screen-lg p-4 mx-auto h-full '>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Contact</p>
+                <p className='py-6'>Submit the form below to contact me</p>
             </div>
-
-            <form action=''>
+        <div className='flex justify-center items-center'>
+            <form action='' className='flex flex-col w-full md:w-1/2 py-6'>
                 <input type="text" name='name' placeholder='Enter your name'
                 className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
                 />
                 <input type="text" name='email' placeholder='Enter your email'
-                className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
+                className='p-2 my-4 bg-transparent border-2 rounded-md text-white focus:outline-none'
                 />
-                <textarea name='message' rows={10} className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' ></textarea>
+                <textarea name='message' placeholder="Type your message" rows={10} className='p-2 my-4  bg-transparent border-2 rounded-md text-white focus:outline-none' ></textarea>
                 <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Submit</button>
                
             </form>
+        </div>
         </div>
 
     </div>
