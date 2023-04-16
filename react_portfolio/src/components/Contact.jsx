bef2ed4bcda5bf54deb48169f967cc0d0d2b4a84
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-16 17:37:34 -0400

    Styling for form

--- react_portfolio/src/components/Contact.jsx
+++ react_portfolio/src/components/Contact.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+        <div>
+            <div>
+                <p>Contact</p>
+                <p>Submit the form below to contact me</p>
+            </div>
+
+            <form action=''>
+                <input type="text" name='name' placeholder='Enter your name'
+                className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
+                />
+                <input type="text" name='email' placeholder='Enter your email'
+                className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
+                />
+                <textarea name='message' rows={10} className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' ></textarea>
+                <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Submit</button>
+               
+            </form>
+        </div>
+
+    </div>
   )
 }
 
