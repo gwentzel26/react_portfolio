9a1eb76728baffa05d368f6c36034fe16ad717cf
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-16 17:07:30 -0400

    Looped over array

--- react_portfolio/src/components/Experience.jsx
+++ react_portfolio/src/components/Experience.jsx
@@ -78,10 +78,15 @@ const Experience = () => {
             </div>
 
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
-                <div>
-                    <img src={html} alt="html" className='w-20 mx-auto' />
-                    <p className='mt-4'>HTML</p>
-                </div>
+                {
+                    techs.map(({id, src, title, style }) => (
+                        <div key={id} className='shadow-md hover:scale-105 duration-500 py-2 rounded-lg'>
+                            <img src={html} alt="html" className='w-20 mx-auto' />
+                             <p className='mt-4'>HTML</p>
+                        </div>
+                    ))
+                }
+             
             </div>
         </div>
         </div>
