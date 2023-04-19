3f7972e399e14014958581cb6ab4ad247193335b
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-19 15:00:18 -0400

    Re-doing contact page with react-hook-form

---
+++ react_portfolio/src/components/ContactForm.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+
+
+const ContactForm = () => {
+const {
+    register, trigger, formState: {errors}
+} = useForm();
+
+
+const onSubmit = async(e) => {
+    const isValid = await trigger();
+    if(!isValid) {
+        e.preventDefault();
+    }
+}
+
+return (
+<section name="Contact" className="w-full h-[140vh] md:h-screen bg-gradient-to-b from-black to-blue-800 p-4 pt-24 md:pt-0 text-white">
+      {/* HEADINGS */}
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 2 }}
+        variants={{
+          hidden: { opacity: 0, x: 50 },
+          visible: { opacity: 1, x: 0 },
+        }}
+        className="flex justify-center w-full"
+      >
+        <div>
+          <p className="font-semibold text-4xl">
+            <span className="text-yellow-500">CONTACT ME</span> <span className="border-b-4 border-blue-300">TO GET STARTED</span>
+          </p>
+          <div className="flex md:justify-end my-5 bg-gradient-rainblue">
+            
+          </div>
+        </div>
+      </motion.div>
+
+      {/* FORM & IMAGE */}
+      <div className="md:flex md:justify-center gap-16 mt-5">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          className="basis-1/2 mt-10 md:mt-0"
+        >
+          <form
+            target="_blank"
+            onSubmit={onSubmit}
+            action="https://formsubmit.co/e8a5bdfa807605332f809e5656e27c6e"
+            method="POST"
+          >
+            <input
+              className="w-full bg-blue font-semibold placeholder-opaque-black p-3"
+              type="text"
+              placeholder="NAME"
+              {...register("name", {
+                required: true,
+                maxLength: 100,
+              })}
+            />
+            {errors.name && (
+              <p className="text-red mt-1">
+                {errors.name.type === "required" && "This field is required."}
+                {errors.name.type === "maxLength" && "Max length is 100 char."}
+              </p>
+            )}
+
+            <input
+              className="w-full bg-blue font-semibold placeholder-opaque-black p-3 mt-5"
+              type="text"
+              placeholder="EMAIL"
+              {...register("email", {
+                required: true,
+                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              })}
+            />
+            {errors.email && (
+              <p className="text-red mt-1">
+                {errors.email.type === "required" && "This field is required."}
+                {errors.email.type === "pattern" && "Invalid email address."}
+              </p>
+            )}
+
+            <textarea
+              className="w-full bg-blue font-semibold placeholder-opaque-black p-3 mt-5"
+              name="message"
+              placeholder="MESSAGE"
+              rows="4"
+              cols="50"
+              {...register("message", {
+                required: true,
+                maxLength: 2000,
+              })}
+            />
+            {errors.message && (
+              <p className="text-red mt-1">
+                {errors.message.type === "required" &&
+                  "This field is required."}
+                {errors.message.type === "maxLength" &&
+                  "Max length is 2000 char."}
+              </p>
+            )}
+
+            <button
+              className="bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300"
+              type="submit"
+            >
+              SEND MESSAGE
+            </button>
+          </form>
+        </motion.div>
+      </div>
+    </section>
+)
+            }
+export default ContactForm
