b5176dac3e43bc21b5ec1ed0ab1bab9a02af6b29
Gavin Wentzel <gavinwentzel@gmail.com>
2023-04-19 15:07:49 -0400

    Added animations using framer motion

--- react_portfolio/src/components/ContactForm.jsx
+++ react_portfolio/src/components/ContactForm.jsx
@@ -46,7 +46,7 @@ return (
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.2, duration: 0.5 }}
+          transition={{ delay: 0.2, duration: 1 }}
           variants={{
             hidden: { opacity: 0, y: 50 },
             visible: { opacity: 1, y: 0 },
